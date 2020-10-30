581a42ff40f592f7011a435111f3967aaf84606e
DanielDamiano <35067590+DanielDamiano@users.noreply.github.com>
2020-10-30 14:31:21 -0400

    Add files via upload

---
+++ Code/EnemyAttack.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 1;
+    //float attackTimerInitial=0;
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    bool playerInRange;
+    float timer;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+
+    }
+
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider other)
+    {
+       if(other.gameObject == player)
+        {
+            playerInRange = true;
+            //attackTimerInitial += Time.deltaTime;
+        }
+    }
+    void OnTriggerExit (Collider other)
+    {
+       if(other.gameObject == player)
+        {
+            playerInRange = false;
+            //Debug.Log(attackTimerInitial);
+        }
+//attackTimerInitial = 0;
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        //if (attackTimerInitial >= 5)
+        //{
+            if (timer >= timeBetweenAttacks && playerInRange)
+            {
+
+                Attack();
+            }
+        //}
+        if(playerHealth.health <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+    }
+    void Attack()
+    {
+        timer = 0f;
+        if (playerHealth.health > 0)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
---
+++ Code/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    UnityEngine.AI.NavMeshAgent nav;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+}
